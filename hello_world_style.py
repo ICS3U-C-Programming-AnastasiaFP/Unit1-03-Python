fcdde35d5c2e178f971b2257d7960cb4d3b1f720
Anastasia Friedenstein-Patino <anastasia.friedenstein-patino@stu.ocsb.ca>
2023-09-16 20:45:19 +0000

    hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created By: Anastasia Friedenstein Patino
+
+# Date: 09 16 2023
+
+# This program displays "Hello, world with style"
+
+def main():
+    print ("Hello, world with style")
+
+if __name__ == "__main__":
+    main()
+
+
